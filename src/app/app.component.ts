111a4f47b6d33fe03d358731d36984fcaf0348d6
vp250031 <vertika.pandey@teradata.com>
2020-07-13 19:10:31 +0530

    Added inputs and todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'to-do-list';
+  title = 'To Do List';
+  taskName = '';
+  taskList = {};
+  taskId = 0;
+  priority = 5;
+
+  addTask(): void {
+    this.taskList[this.taskId++] = 
+    { name: this.taskName, priority: this.priority, isEditMode:false, changedValue: this.taskName };
+  }
+
+  saveChanges(task): void{
+    this.taskList[task.key].name =  this.taskList[task.key].changedValue;
+    this.taskList[task.key].isEditMode = false;
+  }
+
+  removeTask(task):void{
+    delete  this.taskList[task.key];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list'
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
